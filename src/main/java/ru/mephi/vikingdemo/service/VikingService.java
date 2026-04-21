10aaf22c1d492b7f7be7e9c98c29cd8437b484de
Milana--proger <mmilya06@mail.ru>
2026-04-21 09:40:10 +0300

    Service changed

--- src/main/java/ru/mephi/vikingdemo/service/VikingService.java
+++ src/main/java/ru/mephi/vikingdemo/service/VikingService.java
@@ -29,4 +29,49 @@ public class VikingService {
         vikings.add(viking);
         return viking;
     }
+
+    public Viking addViking(Viking viking) {  //добавление конкретного викинга
+        boolean exists = vikings.stream()
+                .anyMatch(v -> v.name().equalsIgnoreCase(viking.name()));
+
+        if (exists) {
+            throw new RuntimeException("Viking with name " + viking.name() + " already exists");
+        }
+
+        vikings.add(viking);
+        return viking;
+    }
+
+    public void deleteViking(String name) { //удаление викинга по имени
+        boolean removed = vikings.removeIf(v -> v.name().equalsIgnoreCase(name));
+
+        if (!removed) {
+            throw new RuntimeException("Viking with name " + name + " not found");
+        }
+    }
+
+    public Viking updateViking(String name, Viking updatedViking) { //перезапись параметров конкретного викинга
+        int index = -1;
+        for (int i = 0; i < vikings.size(); i++) {
+            if (vikings.get(i).name().equalsIgnoreCase(name)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            throw new RuntimeException("Viking with name " + name + " not found");
+        }
+
+        Viking vikingToUpdate = new Viking( //создаем нового викинга с обновленными данными, сохраняя имя из запроса
+                name,
+                updatedViking.age(),
+                updatedViking.heightCm(),
+                updatedViking.hairColor(),
+                updatedViking.beardStyle(),
+                updatedViking.equipment()
+        );
+
+        vikings.set(index, vikingToUpdate);
+        return vikingToUpdate;
 }
